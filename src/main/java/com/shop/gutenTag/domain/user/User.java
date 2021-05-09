cec7369932abd8c4c4905778e9fd4be942e109bf
jeongyuneo <piq2255@gmail.com>
2021-05-09 21:15:06 +0900

    fix: user_id 자동생성 추가

--- src/main/java/com/shop/gutenTag/domain/user/User.java
+++ src/main/java/com/shop/gutenTag/domain/user/User.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Entity
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
 
